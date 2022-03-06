b6bbeaff5f932b9a725b6d81ae2d1f64dab26521
faridhash26 <faridhash26@gmail.com>
2022-03-06 12:37:37 +0330

    initializing commit

---
+++ producer1.py
@@ -0,0 +1,22 @@
+from confluent_kafka import Producer
+
+def acked(err, msg):
+    if err is not None:
+        print("Failed to deliver message: {0}: {1}"
+              .format(msg.value(), err.str()))
+    else:
+        print("Message produced: {0}".format(msg.value()))
+
+p = Producer({'bootstrap.servers': 'localhost:29092'})
+p.produce("mytopic" , key="hello" , value="world")
+
+try:
+    for val in range(1, 1000):
+        p.produce('mytopic', 'myvalue #{0}'
+                  .format(val), callback=acked)
+        p.poll(0.5)
+
+except KeyboardInterrupt:
+    pass
+
+p.flush(30)
